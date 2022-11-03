aaeba79741703362670adb5a3b578ceaff124d1d
orgesa <orgesa_beqiri@yahoo.com>
2022-11-03 10:10:31 +0100

    remove comments

--- src/main/java/com/melita/model/PackageMobile.java
+++ src/main/java/com/melita/model/PackageMobile.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 @Getter
 public enum PackageMobile {
 
-  /*  INTERNET_250 ("INTERNET_250", "Internet 250Mbps"),
+    INTERNET_250 ("INTERNET_250", "Internet 250Mbps"),
     INTERNET_1 ("INTERNET_1", "Internet 1Gbps"),
     TV_90("TV_90", "TV with 90 Channels"),
     TV_140("TV_140", "TV with 140 Channels"),
     TELEPHONY_FREE("TELEPHONY_FREE", "Free on net calls"),
-    TELEPHONY_UNLIMITED("TELEPHONY_UNLIMITED", "Unlimited Calls"),*/
+    TELEPHONY_UNLIMITED("TELEPHONY_UNLIMITED", "Unlimited Calls"),
     MOBILE_POST_PAID("MOBILE", "Post paid"),
     MOBILE_PRE_PAID("MOBILE", "Pree paid");
 
